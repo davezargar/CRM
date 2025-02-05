9d22f6981c702ba8dd4da00748b8ece89c96854b
David Hansén <31345425+davezargar@users.noreply.github.com.>
2025-02-05 14:52:08 +0100

    added .env loading

--- server/Program.cs
+++ server/Program.cs
@@ -1,5 +1,9 @@
+using server;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+DotEnv.Load(Path.Combine(Directory.GetCurrentDirectory(), ".env"));
 
 app.MapGet("/", () => "Hello World!");
 
