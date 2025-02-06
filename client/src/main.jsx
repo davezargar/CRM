d135a766a8918a2373730c6ee8ebbdddf8fc63dd
David Hansén <31345425+davezargar@users.noreply.github.com.>
2025-02-06 16:59:15 +0100

    started on login functionality

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
+import { StrictMode, useState, createContext, use, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route, NavLink} from "react-router";
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+function App()
+{
+    return <BrowserRouter>
+        <Routes>
+            <Route index element={<LoginForm/>}></Route>
+        </Routes>
+    </BrowserRouter>
+}
+
+function LoginForm()
+{
+    function verifyLogin(){
+        
+    }
+    
+    return <form onSubmit={verifyLogin}>
+        <label>email: <input type="text" name="email"/></label>
+        <label>password: <input type="password" name="password"/></label>
+        <input type="submit" value="Sign in"/>
+    </form>
+}
+
