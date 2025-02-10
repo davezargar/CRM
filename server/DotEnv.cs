fa9794ffe840db80d531057d80e73382c0f3347f
David Hansén <31345425+davezargar@users.noreply.github.com.>
2025-02-10 12:42:37 +0100

    login verification, env reader needs refactor on how it splits lines

--- server/DotEnv.cs
+++ server/DotEnv.cs
@@ -4,14 +4,21 @@ public static class DotEnv
 {
     public static void Load(string filePath)
     {
+        Console.WriteLine(filePath);
         if (!File.Exists(filePath))
+        {
+            Console.WriteLine("file not found");
             return;
+        }
+            
 
         foreach (var line in File.ReadAllLines(filePath))
         {
             var parts = line.Split('=', StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine(parts[0] + parts[1]);
             if (parts.Length == 2)
             {
+                Console.WriteLine(parts[0] + parts[1]);
                 Environment.SetEnvironmentVariable(parts[0], parts[1]);
             }
         }
--- server/Queries/DatabaseConnection.cs
+++ server/Queries/DatabaseConnection.cs
@@ -1,6 +1,27 @@
-﻿namespace server.Queries;
+﻿using System.Text;
+using Npgsql;
+namespace server.Queries;
 
 public class DatabaseConnection
 {
-    
+    private NpgsqlDataSource _connection;
+
+    public NpgsqlDataSource Connection()
+    {
+        return _connection;
+    }
+
+    public DatabaseConnection()
+    {
+        if(String.IsNullOrEmpty(Environment.GetEnvironmentVariable("DatabaseConnectString")))
+        {
+            throw new Exception("no connectstring in .env");
+        }
+        
+        Console.WriteLine(Environment.GetEnvironmentVariable("DatabaseConnectString"));
+        _connection = NpgsqlDataSource.Create(Environment.GetEnvironmentVariable("DatabaseConnectString"));
+        
+        Console.WriteLine(Environment.GetEnvironmentVariable("DatabaseConnectString"));
+        using var conn = _connection.OpenConnection();
+    }
 }
