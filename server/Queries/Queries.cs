c6d63983c84336f40e69cd72c509ffb2d4508a6d
David Hansén <31345425+davezargar@users.noreply.github.com.>
2025-02-17 10:58:08 +0100

    removed references to deleted column in tickets

--- server/Queries/Queries.cs
+++ server/Queries/Queries.cs
@@ -126,7 +126,7 @@ public async Task<bool> CreateTicketTask(TicketRequest ticket)
         List<TicketRecord> tickets = new List<TicketRecord>();
         await using var cmd =
             _db.CreateCommand(
-                "SELECT ticket_id, category, subcategory, title, time_posted, time_closed, user_fk, response_email, tickets.company_fk FROM tickets " +
+                "SELECT ticket_id, category, subcategory, title, time_posted, time_closed, user_fk, tickets.company_fk FROM tickets " +
                 "INNER JOIN users ON tickets.company_fk = users.company_fk WHERE email = $1");
         cmd.Parameters.AddWithValue(email);
         using var reader = await cmd.ExecuteReaderAsync();
@@ -141,8 +141,7 @@ public async Task<bool> CreateTicketTask(TicketRequest ticket)
                     reader.GetDateTime(4),
                     reader.IsDBNull(5) ? null : reader.GetDateTime(5),
                     reader.GetString(6),
-                    reader.GetString(7),
-                    reader.GetInt32(8)
+                    reader.GetInt32(7)
                 )
             );
             
--- server/Records/Records.cs
+++ server/Records/Records.cs
@@ -2,4 +2,4 @@
 
 public record LoginRecord(string Email, string Password);
 
-public record TicketRecord(int TicketId, string Category, string Subcategory, string Title, DateTime TimePosted, DateTime? TimeClosed, string UserFk, string ResponseEmail ,int CompanyFk );
+public record TicketRecord(int TicketId, string Category, string Subcategory, string Title, DateTime TimePosted, DateTime? TimeClosed, string UserFk,int CompanyFk );
