350937c1da0e310a9874ba1320c5c9504aee09d3
Tim <tim.bjorkegren@gmail.com>
2025-02-17 12:28:31 +0100

    Fixed the styling for the pop up window

--- client/src/CustomerServicePanel.jsx
+++ client/src/CustomerServicePanel.jsx
@@ -1,10 +1,34 @@
 import "./style/CustomerServicePanel.css"
 
 export default function CustomerServicePanel() {
-    return ReplyButtonDisplay();
+    return DisplayMailWindow();
 }
 
 export function ReplyButtonDisplay() {
-    return <div className="replyContainer"><button id="replyButton">Reply</button></div>
+    return <div className="replyContainer"><button id="replyButton" onClick={DisplayMailWindow}>Reply</button></div>
 
+}
+
+function DisplayMailWindow() {
+    return <div className="mainContainer">
+        <form className="mailContainer">
+
+            <div className="formRow">
+                <label>Title: <input type="text" name="title" required value={Text}></input></label>
+            </div>
+
+            <div className="formRow">
+                <label htmlFor="desc">Description:</label>
+                <textarea name="desc" required rows="4"></textarea>
+
+            </div>
+
+            <div className="checkboxRow">
+                <input type="checkbox" id="checkResolveTickets" name="resolveTickets" value="check"></input>
+                <label for="resolveTickets">Resolve Ticket</label>
+                <button type="submit" id="sendButton">Send</button>
+            </div>
+
+        </form>
+    </div>
 }
