a4519e77d487582780c0d6754b0c6bfa438f9035
David Hansén <31345425+davezargar@users.noreply.github.com>
2025-03-03 11:52:44 +0100

    changes to database from sprint 2 planning

--- server/Program.cs
+++ server/Program.cs
@@ -46,7 +46,7 @@ app.Use(async (context, next) =>
     await next();
 });
 
- #region Routes
+#region Routes
 app.MapPost("/api/workers", async (HttpContext context) =>
 {
     var requestBody = await context.Request.ReadFromJsonAsync<AdminRequest>();
@@ -218,6 +218,7 @@ app.MapPost("/api/customers", async (HttpContext context) =>
 
     return Results.Ok(new { message = "Successfully posted the account to database" });
 });
+
 #endregion
 
 app.Run();
